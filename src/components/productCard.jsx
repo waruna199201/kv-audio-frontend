46d8eccb6e0c51876c9f6f02ee7e006e3bd2a054
waruna199201 <waruna1992@gmail.com>
2025-02-08 09:01:22 +0530

    init😁

---
+++ src/components/productCard.jsx
@@ -0,0 +1,20 @@
+
+import "./productCard.css";
+export default function ProductCard(props){
+
+  
+    return(
+<div>
+    <img src={props.img}/>
+  <span>{props.name}</span>
+  <span1>LKR. {2555}</span1>
+  <p>{props.description}</p>
+  
+  
+<h1>Product Card</h1>
+</div>
+    )
+
+
+}
+
